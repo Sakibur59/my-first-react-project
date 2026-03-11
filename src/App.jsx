30fea9229054513bb64cd7f0ff18844e99f5077b
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-11 21:50:26 +0600

    explore props like- name,devics

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,27 @@ function App() {
       
       <h1>Vite + React</h1>
       <Person></Person>
+      <Developer name="fardin" device="Phone"></Developer>
+      <Developer name="sara" device="laptop"></Developer>
+      <Developer name="ayat" device="PC"></Developer>
       
     </>
   )
 }
 
-
+function Developer(props) {
+  console.log(props)
+  return (
+    <div style={{
+      color:"red",
+      border:"2px solid purple",
+      borderRadius:'20px'
+    }}>
+      <p>Name:{props.name}</p>
+      <p>Devices:{props.device}</p>
+    </div>
+  )
+}
 
 function Person() {
   const personStyle = {
