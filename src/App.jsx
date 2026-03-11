040f7740be27bf6e032afe526a5e52518f6010af
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-11 21:37:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <Person></Person>
+      
+    </>
+  )
+}
+
+function Person() {
+  return(
+    <div>
+
+      <h1>Fardin</h1>
+      <p>CSE Department</p>
+    </div>
+    
+  )
+}
+
+export default App
